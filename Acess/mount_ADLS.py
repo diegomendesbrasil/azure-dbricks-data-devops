892825be38b68ad1e08e5636735bc2dc0e56242b
diegomendesbrasil <50381758+diegomendesbrasil@users.noreply.github.com>
2023-01-09 22:39:48 -0300

    Update mount_ADLS.py

--- Acess/mount_ADLS.py
+++ Acess/mount_ADLS.py
@@ -1,14 +1,14 @@
 # Databricks notebook source
 configs = {"fs.azure.account.auth.type": "OAuth",
-           "fs.azure.account.oauth.provider.type": "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider",
-           "fs.azure.account.oauth2.client.id": "091e16d3-f951-4b4b-8f0e-c5cd0c12d205", 
-           "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="key-vault-secrets",key="DlkDtkKey"), 
-           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/cef04b19-7776-4a94-b89b-375c77a8f936/oauth2/token"} 
+           "fs.azure.account.oauth.provider.type": "",
+           "fs.azure.account.oauth2.client.id": "", 
+           "fs.azure.account.oauth2.client.secret": dbutils.secrets.get(scope="key-vault-secrets",key="cptdaaaa"), 
+           "fs.azure.account.oauth2.client.endpoint": "https://login.microsoftonline.com/abcd-defg-hijk/oauth2/token"} 
 
 # COMMAND ----------
 
 dbutils.fs.mount( 
-  source = "abfss://sap-gb-dlk-de-ascptl@spgbddlkascptl001.dfs.core.windows.net/", 
+  source = "abfss://XPTO.dfs.core.windows.net/", 
   mount_point = "/mnt", 
   extra_configs = configs)
 
